78d206064dfa90ee51f6ace45d91c233b40d8309
Ahmed Ali <121734255+AhmedAli4821@users.noreply.github.com>
2023-02-14 05:39:56 -0800

    JS 14 To 16

---
+++ JS.14 to 20/app.js
@@ -0,0 +1,127 @@
+// var names = ["Ghouse","Ahmed","Ali","Raza"]
+
+// names[0] = "Raza"
+// names[3] = "Gouse"
+// console.log(names)
+
+// var names = ["Ghouse","Ahmed","Ali","Raza"]
+// names [0]=[names[3],names[3]=names[0]][0]
+// console.log(names)
+
+
+// document.write("<h1>Chapter 14 To 16</h1>")
+// document.write("<h3>Question No # 1</h3>")
+
+// var names = ["Gouse"];
+
+// names[1] = "Ahmed"
+// names[2] = "Ali"
+// names[5] = "Raza"
+// console.log(names)
+
+
+// document.write("<h3>Question No # 2</h3>")
+// var object = []
+// console.log(object)
+
+
+
+// document.write("<h3>Question No # 3</h3>")
+
+// var a = ["Ghouse","Ahmed","Ali","Raza"];
+// document.write(a);
+
+
+// document.write("<h3>Question No # 4</h3>")
+
+// var b = [11,30,12,63];
+// document.write(b);
+
+
+// document.write("<h3>Question No # 7</h3>")
+
+
+// var qualifications = ["SSC", "HSC", "BCS","Bs","Bcome","Ms","M Phil","PHD"]
+// document.write("<h3>Qualifications</h3>")
+// document.write(qualifications[0])
+// document.write("<br>")
+// document.write(qualifications[1])
+// document.write("<br>")
+// document.write(qualifications[2])
+// document.write("<br>")
+// document.write(qualifications[3])
+// document.write("<br>")
+// document.write(qualifications[4])
+// document.write("<br>")
+// document.write(qualifications[5])
+// document.write("<br>")
+// document.write(qualifications[6])
+// document.write("<br>")
+// document.write(qualifications[7])
+// document.write("<br>")
+
+// document.write("<h3>Question No # 8</h3>")
+
+// var students = ["Ahmed","Ali","Sajjad"]
+// var marks = [480,465,460]
+// document.write("Score of" +" "+students[0]+" "+ "is" 
+// +" "+marks[0]+" "+"Whose Percentage is"+" "+ marks[0]*100 / 500)
+// document.write("<br>")
+
+// document.write("Score of" +" "+ students[1] +" "+ "is" 
+// +" "+ marks[1] +" "+ "Whose Percentage is" +" "+ marks[1]*100/500)
+
+// document.write("<br>")
+// document.write("Score of" + " " +students[2] + " " +"is" 
+// +" "+ marks[2] +" "+ "Whose Percentage is"+" "+ marks[1]*100/500)
+// document.write("<br>")
+
+// document.write("<h3>Question No # 9 A</h3>")
+
+// var colors = ["red ", "yellow ", "green "]
+// document.write(colors)
+// document.write("<br>")
+// var color = prompt("Enter color")
+// document.write(color + " ," + colors)
+
+// document.write("<h3>Question No # 9 B</h3>")
+
+// var colors = ["red ", "yellow ", "green "]
+// document.write(colors)
+// document.write("<br>")
+// var color = prompt("Enter color")
+// document.write(colors + " ," + color)
+
+// document.write("<h3>Question No # 9 C</h3>")
+
+// var colors = ["red ", "yellow ", "green "]
+// colors.unshift("Pink","Bilow")
+
+// document.write(colors)
+
+
+// document.write("<h3>Question No # 9 D</h3>")
+// var colors = ["red ", "yellow ", "green "]
+// colors.shift()
+// document.write(colors)
+
+// document.write("<h3>Question No # 9 E</h3>")
+
+// var colors = ["red ", "yellow ", "green ","bilow"]
+//  colors.pop()
+//  document.write(colors)
+
+
+//  document.write("<h3>Question No # 9 F</h3>")
+
+//  var colors = ["Red ", "Yellow ", "Green ","Bilow"]
+//  colors.splice(1,0,"Pink")
+//  document.write(colors)
+
+
+ document.write("<h3>Question No # 9 G</h3>")
+
+ var colors = ["Red ", "Yellow ", "Green ","Bilow","Pink"]
+
+colors.splice(3,1)
+document.write(colors)
